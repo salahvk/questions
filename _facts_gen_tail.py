9dc4973f9ce164982c6b1f4fbe2b42a5e6890b1b
salahvk <salahkoyilandy@gmail.com>
2026-06-14 15:44:22 +0530

    update

--- _facts_gen_tail.py
+++
@@ -1,6 +0,0 @@
-# This file is merged into write_facts_modules.py by the build step below.
-# pylint: skip-file
-
-PHYSICS_SCIENTISTS: list[tuple[str, str, list[str], str]] = [
-    ("ഗുരുത്വാകർഷണ നിയമം", "ഐസak ന്യൂട്ടൺ", ["ഗാലിലിയോ", "കെപ്ലർ", "ഫാരaday"], "easy"),
-]
